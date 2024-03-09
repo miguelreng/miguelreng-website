f9ec620617dbb044411f0aa7dded9c3093fb1b5d
Miguel Rengifo <miguelreng@gmail.com>
2024-03-09 23:12:28 +0800

    new code

---
+++ framerusercontent.com/sites/7A4a0iHQYhRcRP4wfjx7Oo/chunk-QSPEQOKW.mjs
@@ -0,0 +1,6 @@
+function e(a,t){return{bodyClassName:"framer-body-ZH4bhOdC6",breakpoints:[{hash:"1c7trwx",mediaQuery:"(min-width: 1200px)"},{hash:"jimtlk",mediaQuery:"(min-width: 810px) and (max-width: 1199px)"},{hash:"13jefmb",mediaQuery:"(min-width: 768px) and (max-width: 809px)"},{hash:"11a6875",mediaQuery:"(max-width: 767px)"}],customHTMLHeadStart:`<style>
+body {
+    background: #333333 !important;
+}
+</style>`,description:"Este libro me cost\xF3 30 d\xEDas para escribirlo, pero la verdad fueron m\xE1s de 2 a\xF1os, de ser alguien que desconozco ahora, de vivir y aprender de la vida. ",elements:{},socialImage:"https://framerusercontent.com/images/uwTUoXEYKREAuTgrpjEkxIFcUU.png",title:"30 poems about life by Miguel Rengifo",viewport:"width=device-width"}}var n=1,d={exports:{metadataVersion:{type:"variable",annotations:{framerContractVersion:"1"}},default:{type:"function",annotations:{framerContractVersion:"1"}},__FramerMetadata__:{type:"variable"}}};export{e as a,n as b,d as c};
+//# sourceMappingURL=chunk-QSPEQOKW.mjs.map
