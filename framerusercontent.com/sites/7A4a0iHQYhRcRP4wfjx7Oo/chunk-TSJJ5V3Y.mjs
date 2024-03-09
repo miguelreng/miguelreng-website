c263fb4adb4e54f512117dc252a98372621c1077
Miguel Rengifo <miguelreng@gmail.com>
2024-03-09 23:04:48 +0800

    j

--- framerusercontent.com/sites/7A4a0iHQYhRcRP4wfjx7Oo/chunk-TSJJ5V3Y.mjs
+++
@@ -1,6 +0,0 @@
-var e=a=>({customHTMLHeadStart:`<style>
-    #__framer-badge-container {
-        display: none;
-    }
-</style>`,description:"Co-Founder of Index96 and Head of Design at Cradle, investing in amazing friends via fund frens, helping designers via firstlayr, building buymyph.",favicon:new URL("https://framerusercontent.com/images/vxvDGJfRcbcDb0lR3lt36eNKlo.png").href,socialImage:new URL("https://framerusercontent.com/images/qxJATkOMxUvBl5XdjT8nwQmh4.png").href,title:"Miguel Rengifo"}),i=e;export{i as a};
-//# sourceMappingURL=chunk-TSJJ5V3Y.mjs.map
