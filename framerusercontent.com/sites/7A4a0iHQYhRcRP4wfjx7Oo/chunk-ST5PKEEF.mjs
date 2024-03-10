05ca5fe280f91040061adfddf00a45a9cdc8add4
Miguel Rengifo <miguelreng@gmail.com>
2024-03-10 10:52:23 +0800

    new structure

---
+++ framerusercontent.com/sites/7A4a0iHQYhRcRP4wfjx7Oo/chunk-ST5PKEEF.mjs
@@ -0,0 +1,2 @@
+import{o as a,p as o}from"./chunk-X5MV5DBS.mjs";var r="default"in o?a:o,t={},e=r;t.createRoot=e.createRoot;t.hydrateRoot=e.hydrateRoot;var s=t.createRoot,f=t.hydrateRoot;export{s as a,f as b};
+//# sourceMappingURL=chunk-ST5PKEEF.mjs.map
