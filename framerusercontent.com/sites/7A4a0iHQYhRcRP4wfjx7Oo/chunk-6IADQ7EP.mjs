fed1f4cdc1c025080d44a6432126df0376fbaf9d
Miguel Rengifo <miguelreng@gmail.com>
2024-03-12 16:51:38 +0800

    c

---
+++ framerusercontent.com/sites/7A4a0iHQYhRcRP4wfjx7Oo/chunk-6IADQ7EP.mjs
@@ -0,0 +1,6 @@
+function e(t,a){return{customHTMLHeadStart:`<style>
+    #__framer-badge-container {
+        display: none;
+    }
+</style>`,description:"I'm Miguel, a senior product designer based in Bali. I currently work designing for early-stage startups/founders through my own design studio called 96.",favicon:"https://framerusercontent.com/images/vxvDGJfRcbcDb0lR3lt36eNKlo.png",socialImage:"https://framerusercontent.com/images/qxJATkOMxUvBl5XdjT8nwQmh4.png",title:"Miguel Rengifo"}}export{e as a};
+//# sourceMappingURL=chunk-6IADQ7EP.mjs.map
---
+++ framerusercontent.com/sites/7A4a0iHQYhRcRP4wfjx7Oo/chunk-IJT2HA7O.mjs
@@ -0,0 +1,6 @@
+function e(a,t){return{bodyClassName:"framer-body-MOSwCGwxw",breakpoints:[{hash:"lz2y3c",mediaQuery:"(min-width: 1200px)"},{hash:"184u6y8",mediaQuery:"(min-width: 810px) and (max-width: 1199px)"},{hash:"zd84bs",mediaQuery:"(max-width: 809px)"}],customHTMLHeadStart:`<style>
+body {
+    background: #333333 !important;
+}
+</style>`,description:"Este libro me cost\xF3 30 d\xEDas para escribirlo, pero la verdad fueron m\xE1s de 2 a\xF1os, de ser alguien que desconozco ahora, de vivir y aprender de la vida. ",elements:{},title:"30 poems about life by Miguel Rengifo",viewport:"width=device-width"}}var d=1,n={exports:{metadataVersion:{type:"variable",annotations:{framerContractVersion:"1"}},default:{type:"function",annotations:{framerContractVersion:"1"}},__FramerMetadata__:{type:"variable"}}};export{e as a,d as b,n as c};
+//# sourceMappingURL=chunk-IJT2HA7O.mjs.map
